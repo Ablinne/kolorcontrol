d00deb5d17f26c7182a539f9234f6354d0bb976b
Alexander Blinne <alexander.blinne@uni-jena.de>
2017-12-01 19:24:19 +0100

    Made sure build_ui is called on sdist,install,develop

--- setup.py
+++ setup.py
@@ -2,7 +2,30 @@ from setuptools import setup
 
 try:
     from pyqt_distutils.build_ui import build_ui
-    cmdclass = {"build_ui": build_ui}
+    from setuptools.command.sdist import sdist
+    from setuptools.command.install import install
+    from setuptools.command.develop import develop
+
+    class custom_sdist(sdist):
+        def run(self):
+            self.run_command('build_ui')
+            return sdist.run(self)
+
+    class custom_develop(develop):
+        def run(self):
+            self.run_command('build_ui')
+            return develop.run(self)
+
+    class custom_install(install):
+        def run(self):
+            self.run_command('build_ui')
+            return install.run(self)
+
+    cmdclass = {"build_ui": build_ui,
+                "sdist": custom_sdist,
+                "develop": custom_develop,
+                "install": custom_install}
+
 except ImportError:
     cmdclass = {}
 
